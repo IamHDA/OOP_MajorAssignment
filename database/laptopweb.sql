5cab7fc11e781f8e4b1cd8e3489dacbe4529bad2
IamHDA <huaduyanh2010@gmail.com>
2024-10-02 23:37:28 +0700

    update

--- database/laptopweb.sql
+++ database/laptopweb.sql
@@ -1,28 +1,27 @@
 CREATE TABLE `User` (
-  `ID` BIGINT,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `Name` NVARCHAR(100),
   `Email` VARCHAR(50),
   `Phone` VARCHAR(11),
-  `Username` VARCHAR(50),
+  `Username` VARCHAR(50) UNIQUE,
   `Pass` VARCHAR(50),
   `RoleID` BIT,
-  `UserAdress` NVARCHAR(100),
-  PRIMARY KEY (`ID`, `Username`)
+  `UserAddress` NVARCHAR(100)
 );
 
 CREATE TABLE `Role` (
   `ID` BIT PRIMARY KEY,
-  `Name` VARCHAR
+  `Name` VARCHAR(9)
 );
 
 CREATE TABLE `Laptop` (
-  `ID` BIGINT PRIMARY KEY,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `SpecID` BIGINT,
   `Name` VARCHAR(100),
   `Brand` VARCHAR(50),
   `Price` INT,
   `Release_Date` DATE,
-  `Status` VARCHAR(30),
+  `Status` BIGINT,
   `Sale` SMALLINT,
   `Available` BOOLEAN
 );
@@ -34,18 +33,18 @@ CREATE TABLE `LaptopCategory` (
 );
 
 CREATE TABLE `Category` (
-  `ID` BIGINT PRIMARY KEY,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `Name` NVARCHAR(100)
 );
 
 CREATE TABLE `Status` (
-  `ID` BIGINT PRIMARY KEY,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `Name` NVARCHAR(50),
   `Description` NVARCHAR(100)
 );
 
 CREATE TABLE `Feedback` (
-  `ID` BIGINT PRIMARY KEY,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `UserID` BIGINT,
   `OrderLapID` BIGINT,
   `Comment` NVARCHAR(255),
@@ -55,13 +54,13 @@ CREATE TABLE `Feedback` (
 );
 
 CREATE TABLE `Shipping_Method` (
-  `ID` BIGINT PRIMARY KEY,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `Name` NVARCHAR(50),
   `Price` INT
 );
 
 CREATE TABLE `Order` (
-  `ID` BIGINT PRIMARY KEY,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `UserID` BIGINT,
   `Shipping_Address` NVARCHAR(100),
   `Status` BIGINT,
@@ -70,7 +69,7 @@ CREATE TABLE `Order` (
 );
 
 CREATE TABLE `Order_Detail` (
-  `ID` BIGINT PRIMARY KEY,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `OrderID` BIGINT,
   `LaptopID` BIGINT,
   `Unit_Price` INT,
@@ -78,58 +77,77 @@ CREATE TABLE `Order_Detail` (
 );
 
 CREATE TABLE `Image` (
-  `ID` BIGINT PRIMARY KEY,
-  `LaptopID` bigint,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
+  `LaptopID` BIGINT,
   `Name` VARCHAR(100),
   `File_Path` VARCHAR(50)
 );
 
 CREATE TABLE `Cart_Detail` (
-  `ID` BIGINT PRIMARY KEY,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `UserID` BIGINT,
   `LaptopID` BIGINT,
   `Quantity` INT
 );
 
 CREATE TABLE `Specification` (
-  `ID` BIGINT PRIMARY KEY,
+  `ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
   `CPU` VARCHAR(30),
   `RAM` VARCHAR(30),
   `ROM` VARCHAR(30),
-  `Sreen` VARCHAR(30),
+  `Screen` VARCHAR(30),
   `GPU` VARCHAR(30),
   `Battery` VARCHAR(20),
   `Weight` FLOAT,
   `Webcam` VARCHAR(30),
   `Operating_System` VARCHAR(20),
   `Connection_Port` VARCHAR(255),
-  `Mux_Switch` BOOLEAN
+  `Mux_Switch` BOOLEAN,
+  `Touch` BOOLEAN
 );
 
-ALTER TABLE `Feedback` ADD FOREIGN KEY (`UserID`) REFERENCES `User` (`ID`);
 
-ALTER TABLE `Cart_Detail` ADD FOREIGN KEY (`UserID`) REFERENCES `User` (`ID`);
+ALTER TABLE `User`
+ADD CONSTRAINT FK_UserRole FOREIGN KEY (`RoleID`) REFERENCES `Role` (`ID`);
 
-ALTER TABLE `Order` ADD FOREIGN KEY (`UserID`) REFERENCES `User` (`ID`);
+ALTER TABLE `Feedback`
+ADD CONSTRAINT FK_FeedbackUser FOREIGN KEY (`UserID`) REFERENCES `User` (`ID`);
 
-ALTER TABLE `Role` ADD FOREIGN KEY (`ID`) REFERENCES `User` (`RoleID`);
+ALTER TABLE `Cart_Detail`
+ADD CONSTRAINT FK_CartUser FOREIGN KEY (`UserID`) REFERENCES `User` (`ID`);
 
-ALTER TABLE `Cart_Detail` ADD FOREIGN KEY (`LaptopID`) REFERENCES `Laptop` (`ID`);
+ALTER TABLE `Cart_Detail`
+ADD CONSTRAINT FK_CartLaptop FOREIGN KEY (`LaptopID`) REFERENCES `Laptop` (`ID`);
 
-ALTER TABLE `Order_Detail` ADD FOREIGN KEY (`OrderID`) REFERENCES `Order` (`ID`);
+ALTER TABLE `Order`
+ADD CONSTRAINT FK_OrderUser FOREIGN KEY (`UserID`) REFERENCES `User` (`ID`);
 
-ALTER TABLE `Status` ADD FOREIGN KEY (`ID`) REFERENCES `Order` (`Status`);
+ALTER TABLE `Order`
+ADD CONSTRAINT FK_OrderStatus FOREIGN KEY (`Status`) REFERENCES `Status` (`ID`);
 
-ALTER TABLE `Shipping_Method` ADD FOREIGN KEY (`ID`) REFERENCES `Order` (`Shipping_Method`);
+ALTER TABLE `Order`
+ADD CONSTRAINT FK_OrderShippingMethod FOREIGN KEY (`Shipping_Method`) REFERENCES `Shipping_Method` (`ID`);
 
-ALTER TABLE `Feedback` ADD FOREIGN KEY (`OrderLapID`) REFERENCES `Order_Detail` (`ID`);
+ALTER TABLE `Order_Detail`
+ADD CONSTRAINT FK_OrderDetailOrder FOREIGN KEY (`OrderID`) REFERENCES `Order` (`ID`);
 
-ALTER TABLE `Order_Detail` ADD FOREIGN KEY (`LaptopID`) REFERENCES `Laptop` (`ID`);
+ALTER TABLE `Order_Detail`
+ADD CONSTRAINT FK_OrderDetailLaptop FOREIGN KEY (`LaptopID`) REFERENCES `Laptop` (`ID`);
 
-ALTER TABLE `Specification` ADD FOREIGN KEY (`ID`) REFERENCES `Laptop` (`SpecID`);
+ALTER TABLE `Feedback`
+ADD CONSTRAINT FK_FeedbackOrderDetail FOREIGN KEY (`OrderLapID`) REFERENCES `Order_Detail` (`ID`);
 
-ALTER TABLE `Image` ADD FOREIGN KEY (`LaptopID`) REFERENCES `Laptop` (`ID`);
+ALTER TABLE `Laptop`
+ADD CONSTRAINT FK_SpecLaptop FOREIGN KEY (`SpecID`) REFERENCES `Specification` (`ID`);
 
-ALTER TABLE `Category` ADD FOREIGN KEY (`ID`) REFERENCES `LaptopCategory` (`CateID`);
+ALTER TABLE `Image`
+ADD CONSTRAINT FK_ImageLaptop FOREIGN KEY (`LaptopID`) REFERENCES `Laptop` (`ID`);
 
-ALTER TABLE `LaptopCategory` ADD FOREIGN KEY (`LaptopID`) REFERENCES `Laptop` (`ID`);
+ALTER TABLE `LaptopCategory`
+ADD CONSTRAINT FK_LaptopCategoryLaptop FOREIGN KEY (`LaptopID`) REFERENCES `Laptop` (`ID`);
+
+ALTER TABLE `LaptopCategory`
+ADD CONSTRAINT FK_LaptopCategoryCategory FOREIGN KEY (`CateID`) REFERENCES `Category` (`ID`);
+
+
+
