f58601850e7e2575b53d054eec721929a001e7cd
phanbien2004 <bien382004@gmail.com>
2024-10-19 14:32:26 +0700

    fe

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -6,9 +6,6 @@ header__top__account__login.addEventListener('click', function(){
     loginBox.style.display = 'block';
 });
 
-// exitRegisterBox.addEventListener('click', function(){
-//     register__box.style.display = 'none';
-// });
 
 exitLoginBox.addEventListener('click', function(){
    loginBox.style.display = 'none';
@@ -19,30 +16,70 @@ register[1].addEventListener('click', function(){
     registerBox.style.display = "block";
 });
 
+var loginButton = document.querySelector("#login__button");
 
+loginButton.addEventListener('click', function(){
+    console.log("Day la nut an dang nhap");
+    var email = document.querySelector('.login__box__email').value;
+    var password = document.querySelector('.login__box__password').value;
 
-// Elenment cua trang
-const myLogin__box = document.getElementById("myLogin__box");
-const Email = document.getElementById("userEmail");
-const yourPassword = document.getElementById("password")
-// Element loi
-const notEmail = document.getElementById("invalidEmail");
-const incorrectEmail = document.getElementById("incorrectEmail");
+    const data = {
+        dataEmail: email,
+        dataUserPassword: password
+    }
 
-myLogin__box.addEventListener("submit", function(e){
-        e.preventDefault;
-    
-        if(!Email.value){
-            notEmail.style.display = "block";
-        } else{
-            notEmail.style.display = "none";
+    // day tai khoan mat khau len server
+
+    fetch('http://localhost:3000/account1234',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) // Chuyển dữ liệu thành JSON
+    })
+
+    // lay trang thai tu server tra ve(User not found/ User login successful)
+
+    fetch('http://localhost:3000/status',{
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer token'
         }
-
-        if(!yourPassword.value){
-            incorrectEmail.style.display = "block";
-        } else {
-            incorrectEmail.style.display = "none";
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(response => {
+        if(response[0] == "User not found"){
+            var war = document.querySelector('#incorrectEmail');
+            war.style.display = "block";
         }
+        else if (response[0] == "User login successful"){
+            var war = document.querySelector('#incorrectEmail');
+            war.style.display = "none";
+            loginBox.style.display = 'none';
+            document.querySelector('.header__top__account').style.display = 'none';
+            document.querySelector('.header__top__login-success').style.display = 'block';
 
-        window.location.href = "../Phần Trang chủ/index.html";
-});
+            var tmp = '';
+            var tmpname = ''
+            // Lay ten 
+            fetch('http://localhost:3000/account', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer token'
+                }
+            })
+            .then(response => {
+                response.json();
+            })
+            .then(response => {
+                tmpname = response.name;
+            })
+            tmp = '<p> Xin chào ' + tmpname + '<p>';
+            document.querySelector('.header__top__login-success').innerHTML = tmp;
+        }
+    })
+})
