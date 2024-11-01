1a06fc16f10d7d7af452c8a66e89e5cdc109c635
phanbien2004 <bien382004@gmail.com>
2024-11-01 23:42:04 +0700

    search

--- frontend/js/product__detail.js
+++ frontend/js/product__detail.js
@@ -97,4 +97,21 @@ fetch(`http://localhost:8080/laptop/${id}`, {
     }
 })
 
+// .then(response =>{
+//     let search = document.querySelector('.search');
+//     let inputSearch = document.querySelector('.input__search');
+//     let buttonSearch = document.querySelector('.button__search');
+
+
+//     buttonSearch.addEventListener('click', function(){
+//         const valueSearch = inputSearch.value;
+//         console.log(valueSearch);
+//         console.log("ok");
+//         localStorage.setItem('valueSearch', valueSearch);
+//         window.location.href = 'laptoptheomuc.html';
+//     })
+// })
+
+
+
 
---
+++ frontend/js/search__value.js
@@ -0,0 +1,12 @@
+let search = document.querySelector('.search');
+let inputSearch = document.querySelector('.input__search');
+let buttonSearch = document.querySelector('.button__search');
+
+
+buttonSearch.addEventListener('click', function(){
+    const valueSearch = inputSearch.value;
+    console.log(valueSearch);
+    console.log("ok");
+    localStorage.setItem('valueSearch', valueSearch);
+    window.location.href = 'laptoptheomuc.html';
+});
