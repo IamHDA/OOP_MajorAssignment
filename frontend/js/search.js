0a14921150e3d94e901d45f8f685dc0e047f592a
IamHDA <huaduyanh2010@gmail.com>
2024-11-02 02:35:36 +0700

    text

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -37,9 +37,28 @@ function searchProduct(){
             var productPrice = '<div class="product__price">' + price + ' đ' + ' </div>'; 
             var productBasePrice = '<div class="product__base__price">' + '<h1>' + basePrice + ' đ' + '</h1>' + '<h2> (Tiết kiệm ' + data[i].sale + '%)</h2>' + '</div>';
             var cart = '<div class="product__cart"> <i class="fa-solid fa-cart-shopping"></i> </div>'
-            var laptopContainer = '<div class="laptop-container">' + idProduct + imgProduct + nameProduct + productPrice +productBasePrice + cart + '</div>';
+            var laptopContainer = '<div class="product__container">' + idProduct + imgProduct + nameProduct + productPrice +productBasePrice + cart + '</div>';
             allLaptop.innerHTML += laptopContainer;
         }
+    })
+    .then(data => {
+        var allLaptop = document.querySelector('.all-laptop');
+        var childrenAllLapTop = Array.from(allLaptop.children);
+        
+        childrenAllLapTop.forEach(function(element) {
+            var productImg = element.getElementsByTagName('img');
+            var productName = element.querySelector('.product__name');
+            var productId = element.querySelector(".id__product");
+
+            productImg.addEventListener('click', function() {
+                localStorage.setItem('id__product', productId.textContent);
+            });
+            productName.addEventListener('click', function() {
+                localStorage.setItem('id__product', productId.textContent);
+            });
+
+            console.log(productId.textContent);
+        });
     })
     .catch(error => console.log(error));
 }
