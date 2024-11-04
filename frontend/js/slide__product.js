d4957d4f69cf9e0e1e805219428b525fd6b9189e
phanbien2004 <bien382004@gmail.com>
2024-11-04 13:24:57 +0700

    adjust__cart_detail

--- frontend/js/slide__product.js
+++ frontend/js/slide__product.js
@@ -435,20 +435,22 @@ function luotSlide(){
 }
 
 function selectProduct(){
-    var allLaptop = document.querySelector('.all-laptop');
-    var childrenAllLapTop = Array.from(allLaptop.children);
-    
-    childrenAllLapTop.forEach(function(element){
-        var productImg = element.getElementsByTagName('img')[0];
-        var productName = element.querySelector('.product__name');
-        var productId = element.querySelector(".id__product");
+    var AllLaptop = document.querySelectorAll('.all-laptop');
+    AllLaptop.forEach(function(allLaptop){
+        var childrenAllLapTop = Array.from(allLaptop.children);
+        
+        childrenAllLapTop.forEach(function(element){
+            var productImg = element.getElementsByTagName('img')[0];
+            var productName = element.querySelector('.product__name');
+            var productId = element.querySelector(".id__product");
 
-        productImg.addEventListener('click', function(){
-            localStorage.setItem('id__product', productId.textContent);
-        });
-        productName.addEventListener('click', function(){
-            localStorage.setItem('id__product', productId.textContent);
-        });
+            productImg.addEventListener('click', function(){
+                localStorage.setItem('id__product', productId.textContent);
+            });
+            productName.addEventListener('click', function(){
+                localStorage.setItem('id__product', productId.textContent);
+            });
+        })
     })
 }
 
