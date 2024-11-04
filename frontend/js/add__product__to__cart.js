0c5d474ba4f69d4582cc8665199b3c8b8a503457
phanbien2004 <bien382004@gmail.com>
2024-11-04 22:08:19 +0700

    fe

--- frontend/js/add__product__to__cart.js
+++ frontend/js/add__product__to__cart.js
@@ -1,6 +1,35 @@
+async function fetchAddProductToCart(idProduct){
+    try {
+        const data = {
+            laptop_id: idProduct
+        }
+        const response = await fetch('http://localhost:8080/cart-detail/add', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            },
+            body: JSON.stringify(data)    
+        });
+
+        response = await response.json();
+        return response;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+
 function addProductToCart(){
     let productCotainer = document.querySelectorAll('.product__container');
     productCotainer.forEach(function(element){
-        let 
+        let butonAddToCart = element.querySelector('.product__cart');
+        let idProduct = element.querySelector('.id__product');
+        butonAddToCart.addEventListener('click', function(){
+            fetchAddProductToCart(idProduct);
+        })
     })
 }
+
+
+
