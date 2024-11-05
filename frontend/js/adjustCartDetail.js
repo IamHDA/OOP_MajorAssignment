09b7c3fc6a58357ecd7c88405e181f6ed0ac43dd
phanbien2004 <bien382004@gmail.com>
2024-11-05 16:45:32 +0700

    dsdf

--- frontend/js/adjustCartDetail.js
+++ frontend/js/adjustCartDetail.js
@@ -26,66 +26,29 @@ function daucham(num) {
 }
 
 // -------------------------------------------
-let cartDetail = document.querySelector('.my-cart-detail');
-let emptyCart = document.querySelector('.empty-cart');
-let tableRow = document.querySelectorAll('.table-row');
-if (tableRow.length != 0){
-    cartDetail.style.display = 'flex';
-    emptyCart.style.display = 'none';
-    function adjustNumberProduct(){
-        let totalPrice = document.querySelector('.total-price');
-        tableRow.forEach(function(element){
-            let buttonRight = element.querySelector('.right-button');
-            let numberProduct = element.querySelector('.laptop-counter');
-            let buttonLeft = element.querySelector('.left-button');
-            let unitPrice = element.querySelector('.unit-price');
-            let totalUnitPrice = element.querySelector('.total-unit-price');
 
-            // Chỉnh màu cho nút giảm khi sản phẩm bằng 1 và khác 1
-            if(numberProduct.textContent == "1"){
-                buttonLeft.style.color = '#D4D1D1';
-            }
+function adjustNumberProduct(){
+    let totalPrice = document.querySelector('.total-price');
+    tableRow.forEach(function(element){
+        let buttonRight = element.querySelector('.right-button');
+        let numberProduct = element.querySelector('.laptop-counter');
+        let buttonLeft = element.querySelector('.left-button');
+        let unitPrice = element.querySelector('.unit-price');
+        let totalUnitPrice = element.querySelector('.total-unit-price');
+
+        // Chỉnh màu cho nút giảm khi sản phẩm bằng 1 và khác 1
+        if(numberProduct.textContent == "1"){
+            buttonLeft.style.color = '#D4D1D1';
+        }
+        if(numberProduct.textContent != "1"){
+            buttonLeft.style.color = 'black';
+        }
+
+        // Bấm nút giảm
+        buttonLeft.addEventListener('click', function(){
             if(numberProduct.textContent != "1"){
-                buttonLeft.style.color = 'black';
-            }
-
-            // Bấm nút giảm
-            buttonLeft.addEventListener('click', function(){
-                if(numberProduct.textContent != "1"){
-                    let currentNumber = parseInt(numberProduct.textContent, 10);
-                    let newNumber = currentNumber - 1;
-                    newNumber = newNumber.toString();
-                    numberProduct.innerHTML = newNumber;
-                    // Chỉnh giá
-                    let unitPriceNumber = unitPrice.textContent; // gia 1 product
-                    unitPriceNumber = boDauCham(unitPriceNumber);
-                    unitPriceNumber = parseInt(unitPriceNumber, 10); // chuyen gia 1 product ve int
-                    let totalUnitPriceNumber = unitPriceNumber * parseInt(newNumber, 10); // tinh tong gia moi
-                    totalUnitPriceNumber = totalUnitPriceNumber.toString(); // chuyen tong gia moi ve string
-                    totalUnitPriceNumber = daucham(totalUnitPriceNumber) + " VNĐ";
-                    totalUnitPrice.innerHTML = totalUnitPriceNumber;
-
-                    // Chỉnh màu cho nút giảm khi sản phẩm bằng 1
-                    if(numberProduct.textContent == "1"){
-                        buttonLeft.style.color = '#D4D1D1';
-                    }
-
-                    // Chinh tong gia cart
-                    let totalPriceNumber = totalPrice.textContent;
-                    totalPriceNumber = boDauCham(totalPriceNumber);
-                    totalPriceNumber = parseInt(totalPriceNumber, 10);
-                    totalPriceNumber -= unitPriceNumber;
-                    totalPriceNumber = totalPriceNumber.toString();
-                    totalPriceNumber = daucham(totalPriceNumber) + " VNĐ";
-                    totalPrice.innerHTML = totalPriceNumber;
-                }    
-            })
-
-            // Bấm nút tăng
-
-            buttonRight.addEventListener('click', function(){
                 let currentNumber = parseInt(numberProduct.textContent, 10);
-                let newNumber = currentNumber + 1;
+                let newNumber = currentNumber - 1;
                 newNumber = newNumber.toString();
                 numberProduct.innerHTML = newNumber;
                 // Chỉnh giá
@@ -97,29 +60,55 @@ if (tableRow.length != 0){
                 totalUnitPriceNumber = daucham(totalUnitPriceNumber) + " VNĐ";
                 totalUnitPrice.innerHTML = totalUnitPriceNumber;
 
-                // Chỉnh màu cho nút giảm khi sản phẩm khac 1
-                if(numberProduct.textContent != "1"){
-                    buttonLeft.style.color = 'black';
+                // Chỉnh màu cho nút giảm khi sản phẩm bằng 1
+                if(numberProduct.textContent == "1"){
+                    buttonLeft.style.color = '#D4D1D1';
                 }
 
                 // Chinh tong gia cart
                 let totalPriceNumber = totalPrice.textContent;
-                totalPriceNumber = boDauCham(totalPriceNumber); 
+                totalPriceNumber = boDauCham(totalPriceNumber);
                 totalPriceNumber = parseInt(totalPriceNumber, 10);
-                totalPriceNumber += unitPriceNumber;
+                totalPriceNumber -= unitPriceNumber;
                 totalPriceNumber = totalPriceNumber.toString();
                 totalPriceNumber = daucham(totalPriceNumber) + " VNĐ";
                 totalPrice.innerHTML = totalPriceNumber;
-            })
-
-
+            }    
         })
-    }
-}
 
-if (tableRow.length == 0){
-    cartDetail.style.display = 'none';
-    emptyCart.style.display = 'block';
+        // Bấm nút tăng
+
+        buttonRight.addEventListener('click', function(){
+            let currentNumber = parseInt(numberProduct.textContent, 10);
+            let newNumber = currentNumber + 1;
+            newNumber = newNumber.toString();
+            numberProduct.innerHTML = newNumber;
+            // Chỉnh giá
+            let unitPriceNumber = unitPrice.textContent; // gia 1 product
+            unitPriceNumber = boDauCham(unitPriceNumber);
+            unitPriceNumber = parseInt(unitPriceNumber, 10); // chuyen gia 1 product ve int
+            let totalUnitPriceNumber = unitPriceNumber * parseInt(newNumber, 10); // tinh tong gia moi
+            totalUnitPriceNumber = totalUnitPriceNumber.toString(); // chuyen tong gia moi ve string
+            totalUnitPriceNumber = daucham(totalUnitPriceNumber) + " VNĐ";
+            totalUnitPrice.innerHTML = totalUnitPriceNumber;
+
+            // Chỉnh màu cho nút giảm khi sản phẩm khac 1
+            if(numberProduct.textContent != "1"){
+                buttonLeft.style.color = 'black';
+            }
+
+            // Chinh tong gia cart
+            let totalPriceNumber = totalPrice.textContent;
+            totalPriceNumber = boDauCham(totalPriceNumber); 
+            totalPriceNumber = parseInt(totalPriceNumber, 10);
+            totalPriceNumber += unitPriceNumber;
+            totalPriceNumber = totalPriceNumber.toString();
+            totalPriceNumber = daucham(totalPriceNumber) + " VNĐ";
+            totalPrice.innerHTML = totalPriceNumber;
+        })
+
+
+    })
 }
 
 function deleteProduct(){
@@ -172,7 +161,3 @@ function deleteAllProduct(){
     })
 }
 
-
-adjustNumberProduct();
-deleteProduct();
-deleteAllProduct();
