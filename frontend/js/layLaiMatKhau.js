25810f0e528461a481f55084454f81055314de85
IamHDA <huaduyanh2010@gmail.com>
2024-11-09 15:16:51 +0700

    fix fe

--- frontend/js/layLaiMatKhau.js
+++ frontend/js/layLaiMatKhau.js
@@ -4,28 +4,25 @@ async function layLaiMatKhau(){
     button.addEventListener("click", async function(){
         let email = quenMatKhau.querySelector("input").value;
         try{
-            let response1 = await fetch('http://localhost:8080/forgetPassword/checkEmail',{
+            let response1 = await fetch(`http://localhost:8080/forgetPassword/checkEmail/${email}`,{
                 method: 'GET',
                 headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`
-                },
-                body: email
+                'Content-Type': 'application/json'
+                }
             });
             response1 = await response1.text();
             if(response1 === "User found"){
                 quenMatKhau.querySelector('.war').style.display = "none";
                 try{
-                    let response2 = await fetch('http://localhost:8080/forgetPassword/sendEmail',{
+                    alert("Yêu cầu của bạn đang được xử lý, vui lòng đợi!");
+                    let response2 = await fetch(`http://localhost:8080/forgetPassword/sendMail/${email}`,{
                         method: 'GET',
                         headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${accessToken}`
-                        },
-                        body: email
+                        'Content-Type': 'application/json'
+                        }
                     });
-                    response2 = await response2.json();
-                    if(response2 === "Mail sent successfuly"){
+                    response2 = await response2.text();
+                    if(response2 == "Email sent successfully"){
                         alert("Yêu cầu lấy lại mật khẩu của bạn cần được xác minh. Hãy vào kiểm tra Email của bạn để xác nhận đổi mật khẩu!");
                     }
                 }
