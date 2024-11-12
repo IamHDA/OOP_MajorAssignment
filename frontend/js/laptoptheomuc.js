eb5ad083b8e6a4ee63b2bd1278c79db4c6553886
phanbien2004 <bien382004@gmail.com>
2024-11-12 14:10:23 +0700

    fe

--- frontend/js/laptoptheomuc.js
+++ frontend/js/laptoptheomuc.js
@@ -1,124 +1,122 @@
-import selectProduct from "./selectProduct.js";
-
-function lamtron(num) {
-    return Math.round(num / 100000) * 100000;
-}
-
-function daucham(num){
-    let tmp = "";
-    let mark = 0;
-    for(let i = num.length - 1; i >= 0; i--){
-        mark += 1;
-        tmp = num[i] + tmp;
-        if(mark == 3 && i != 0){
-            tmp = "." + tmp;
-            mark = 0
+function searchProduct(){
+    function lamtron(num) {
+        return Math.round(num / 100000) * 100000;
+    }
+    
+    function daucham(num){
+        let tmp = "";
+        let mark = 0;
+        for(let i = num.length - 1; i >= 0; i--){
+            mark += 1;
+            tmp = num[i] + tmp;
+            if(mark == 3 && i != 0){
+                tmp = "." + tmp;
+                mark = 0
+            }
         }
+        return tmp;
     }
-    return tmp;
+
+    let keyword = localStorage.getItem('valueSearch');
+    fetch(`http://localhost:8080/laptop/search?keyword=${keyword}`)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        let allLaptop = document.querySelector('.all-laptop');
+        for(let i = 0; i < data.length; i++){
+            var idProduct = '<div class="id__product">' + data[i].id + '</div>';
+            var imgProduct = '<a href="product.html" class="produc__img">'+ '<img src="' + data[i].images[0].filePath + '.jpg' + '" alt="">'+ '</a>';
+            var nameProduct = '<a href="product.html" class="product__name">' + data[i].name + ' ' + data[i].specification.cpu + ' ' + data[i].specification.ram + ' ' + data[i].specification.graphicsCard + '</a>';
+            let basePrice = data[i].price.toString();
+            basePrice = daucham(basePrice);
+            let price = data[i].price * (100 - data[i].sale) / 100;
+            price = lamtron(price);
+            price = price.toString();
+            price = daucham(price);
+            var productPrice = '<div class="product__price">' + price + ' đ' + ' </div>'; 
+            var productBasePrice = '<div class="product__base__price">' + '<h1>' + basePrice + ' đ' + '</h1>' + '<h2> (Tiết kiệm ' + data[i].sale + '%)</h2>' + '</div>';
+            var cart = '<div class="product__cart"> <i class="fa-solid fa-cart-shopping"></i> </div>'
+            var laptopContainer = '<div class="product__container">' + idProduct + imgProduct + nameProduct + productPrice +productBasePrice + cart + '</div>';
+            allLaptop.innerHTML += laptopContainer;
+        }
+    })
+    .catch(error => console.log(error));
 }
 
-// buildSearchProduct
-async function buildSearchProduct(dataResponse){
-    let allLaptop = document.querySelector('.all-laptop');
-    for(let i = 0; i < dataResponse.length; i++){
-        var idProduct = '<div class="id__product">' + dataResponse[i].id + '</div>';
-        var imgProduct = '<a href="product.html" class="produc__img">'+ '<img src="' + dataResponse[i].images[0].filePath  + '" alt="">'+ '</a>';
-        var nameProduct = '<a href="product.html" class="product__name">' + dataResponse[i].name + '( ' + dataResponse[i].specification.cpu + ', ' + dataResponse[i].specification.graphicsCard + ', ' + dataResponse[i].specification.ram + ', ' + dataResponse[i].specification.rom +', '+ dataResponse[i].specification.screen + ' )' + '</a>';
-        let basePrice = dataResponse[i].price.toString();
-        basePrice = daucham(basePrice);
-        let price = dataResponse[i].price * (100 - dataResponse[i].sale) / 100;
-        price = lamtron(price);
-        price = price.toString();
-        price = daucham(price);
-        var productPrice = '<div class="product__price">' + price + ' đ' + ' </div>'; 
-        var productBasePrice = '<div class="product__base__price">' + '<h1>' + basePrice + ' đ' + '</h1>' + '<h2> (Tiết kiệm ' + dataResponse[i].sale + '%)</h2>' + '</div>';
-        var cart = '<div class="product__cart"> <i class="fa-solid fa-cart-shopping"></i> </div>'
-        var laptopContainer = '<div class="product__container">' + idProduct + imgProduct + nameProduct + productPrice +productBasePrice + cart + '</div>';
-        allLaptop.innerHTML += laptopContainer;
-    }
-}
-
-async function searchProduct(){
-    try{
-        let keyword = localStorage.getItem('valueSearch');
-        const response = await fetch(`http://localhost:8080/laptop/api/search?keyword=${keyword}`)
-        const dataResponse =  await response.json();
-
-        await buildSearchProduct(dataResponse);
-    }
-    catch (error) {
-        console.error('Error fetching data:', error);
-    }
-}
-
-//buildSelectCategoryProduct
-async function buildSelectCategoryProduct(data) {
-    let allLaptop = document.querySelector('.all-laptop');
-    for(let i = 0; i < data.length; i++){
-        var idProduct = '<div class="id__product">' + data[i].id + '</div>';
-        var imgProduct = '<a href="product.html" class="produc__img">'+ '<img src="' + data[i].images[0].filePath +  '" alt="">'+ '</a>';
-        var nameProduct = '<a href="product.html" class="product__name">' + data[i].name + ' ' + data[i].specification.cpu + ' ' + data[i].specification.ram + ' ' + data[i].specification.graphicsCard + '</a>';
-        let basePrice = data[i].price.toString();
-        basePrice = daucham(basePrice);
-        let price = data[i].price * (100 - data[i].sale) / 100;
-        price = lamtron(price);
-        price = price.toString();
-        price = daucham(price);
-        var productPrice = '<div class="product__price">' + price + ' đ' + ' </div>'; 
-        var productBasePrice = '<div class="product__base__price">' + '<h1>' + basePrice + ' đ' + '</h1>' + '<h2> (Tiết kiệm ' + data[i].sale + '%)</h2>' + '</div>';
-        var cart = '<div class="product__cart"> <i class="fa-solid fa-cart-shopping"></i> </div>'
-        var laptopContainer = '<div class="product__container">' + idProduct + imgProduct + nameProduct + productPrice +productBasePrice + cart + '</div>';
-        allLaptop.innerHTML += laptopContainer;
-    }
-}
-
-async function selectCategoryProduct(){
+function selectCategoryProduct(){
     let branch = localStorage.getItem('branch');
     let state = localStorage.getItem('state');
     let api = 'http://localhost:8080/collections/filter?category=' + branch + '&brand=&cpu&vga&ram&ssd&screenSize&state=' + state + '&sortBy&sortOrder&minPrice=0&maxPrice=0';    
-
-    try{
-        const response = await fetch(`${api}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }       
-        })
-
-        data = await response.json();
-        await buildSelectCategoryProduct(data);
+    function lamtron(num) {
+        return Math.round(num / 100000) * 100000;
     }
-    catch (error) {
-        console.error('Error fetching data:', error);
+    
+    function daucham(num){
+        let tmp = "";
+        let mark = 0;
+        for(let i = num.length - 1; i >= 0; i--){
+            mark += 1;
+            tmp = num[i] + tmp;
+            if(mark == 3 && i != 0){
+                tmp = "." + tmp;
+                mark = 0
+            }
+        }
+        return tmp;
     }
-}
 
-async function filter() {
-    let option = document.querySelectorAll('.option');
-    option.forEach(function(element){
-        let choices = element.querySelector('.choices');
-        let choice = choices.querySelectorAll('a');
-        choice.forEach(function(ele){
-            ele.addEventListener('click', function(){
-                localStorage.setItem('thuongHieu', ele.textContent);
-            })
-        })
+    fetch(`${api}`)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        let allLaptop = document.querySelector('.all-laptop');
+        for(let i = 0; i < data.length; i++){
+            var idProduct = '<div class="id__product">' + data[i].id + '</div>';
+            var imgProduct = '<a href="product.html" class="produc__img">'+ '<img src="' + data[i].images[0].filePath + '.jpg' + '" alt="">'+ '</a>';
+            var nameProduct = '<a href="product.html" class="product__name">' + data[i].name + ' ' + data[i].specification.cpu + ' ' + data[i].specification.ram + ' ' + data[i].specification.graphicsCard + '</a>';
+            let basePrice = data[i].price.toString();
+            basePrice = daucham(basePrice);
+            let price = data[i].price * (100 - data[i].sale) / 100;
+            price = lamtron(price);
+            price = price.toString();
+            price = daucham(price);
+            var productPrice = '<div class="product__price">' + price + ' đ' + ' </div>'; 
+            var productBasePrice = '<div class="product__base__price">' + '<h1>' + basePrice + ' đ' + '</h1>' + '<h2> (Tiết kiệm ' + data[i].sale + '%)</h2>' + '</div>';
+            var cart = '<div class="product__cart"> <i class="fa-solid fa-cart-shopping"></i> </div>'
+            var laptopContainer = '<div class="product__container">' + idProduct + imgProduct + nameProduct + productPrice +productBasePrice + cart + '</div>';
+            allLaptop.innerHTML += laptopContainer;
+        }
     })
 }
 
-// main
-async function mainLapTopTheoMuc(){
+function selectProduct(){
+    var allLaptop = document.querySelector('.all-laptop');
+    var childrenAllLapTop = Array.from(allLaptop.children);
     
-    if(localStorage.getItem('action') === "search"){
-        await searchProduct();
-    }
-    
-    if(localStorage.getItem('action') === "selectionCategory"){
-        await selectCategoryProduct();
-    }
+    childrenAllLapTop.forEach(function(element){
+        var productImg = element.getElementsByTagName('img');
+        var productName = element.querySelector('.product__name');
+        var productId = element.querySelector(".id__product");
 
-    selectProduct();
+        productImg.addEventListener('click', function(){
+            localStorage.setItem('id__product', productId.textContent);
+        });
+        productName.addEventListener('click', function(){
+            localStorage.setItem('id__product', productId.textContent);
+        });
+    })
 }
 
-mainLapTopTheoMuc();
+
+
+if(localStorage.getItem('action') === "search"){
+    searchProduct();
+}
+
+if(localStorage.getItem('action') === "selectionCategory"){
+    selectCategoryProduct();
+}
+
+selectProduct();
