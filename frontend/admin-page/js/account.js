08195e25c0007d54ebbef0b2af3942de775e3ccb
phanbien2004 <bien382004@gmail.com>
2024-11-16 13:04:13 +0700

    page

--- frontend/admin-page/js/account.js
+++ frontend/admin-page/js/account.js
@@ -17,9 +17,11 @@ async function getAllAccount(){
 async function buildAllAccount() {
     let data = await getAllAccount();
     let index = 1;
-    let accountTable = document.querySelector('.account-table');
+    let accountTable;
+    let pages = document.querySelector('.pages');
+    let numberRow = 0;
     await data.forEach(function(element){
-        console.log(element);
+        numberRow += 1;
         let id = '<td class="id">' + element.id + '</td>';
         let stt =  '<td class="stt">' + index + '</td>';
         index += 1;
@@ -30,7 +32,12 @@ async function buildAllAccount() {
         let registrationDate = '<td class="registration-date">' + element.registrationDate + '</td>';
         let role = '<td class="role">' + element.role + '</td>';
         let tableRow = '<tr class="table-other-row">' + id + stt + name + email + contact + address + registrationDate + role + '</tr>';
-        accountTable.innerHTML += tableRow;
+        accountTable += tableRow;
+        if(numberRow == 5){
+            accountTable = '<table class="account-table"><tr class="table-first-row"><td class="stt">STT</td><td class="id">ID</td><td class="name">Họ tên</td><td class="email">Email</td><td class="contact">Số điện thoại</td><td class="address">Địa chỉ</td><td class="registration-date">Ngày đăng ký</td><td class="role">Vai trò</td></tr>' + accountTable + '</table>';
+            pages.innerHTML += accountTable;
+            accountTable = '';
+        }
     })
 }
 
