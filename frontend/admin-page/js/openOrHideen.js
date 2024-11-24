85868d98a2de6ecfa67779607cd422f6aa15d42d
IamHDA <huaduyanh2010@gmail.com>
2024-11-24 21:34:53 +0700

    add more laptop and modify laptop control

--- frontend/admin-page/js/openOrHideen.js
+++ frontend/admin-page/js/openOrHideen.js
@@ -3,19 +3,24 @@ let subOption = option2.querySelector('.sub-option');
 
 if(localStorage.getItem('hidden') == "true"){
     subOption.style.display = 'flex';
+    subOption.style.marginTop = "10px";
+    option2.style.paddingBottom = "0px";
 }
 
 if(localStorage.getItem('hidden') == "false"){
     subOption.style.display = 'none';
+    option2.style.paddingBottom = "10px";
 }
 
 option2.addEventListener('click', function(){
     if(localStorage.getItem('hidden') == "true"){
-        localStorage.getItem('hidden', 'false');
-        subOption.style.display = 'false';
-    }
-    if(localStorage.getItem('hidden') == "false"){
-        localStorage.getItem('hidden', 'true');
+        localStorage.setItem('hidden', 'false');
+        subOption.style.display = 'flex';
+        subOption.style.marginTop = "10px";
+        option2.style.paddingBottom = "0px";
+    }else if(localStorage.getItem('hidden') == "false"){
+        localStorage.setItem('hidden', 'true');
         subOption.style.display = 'none';
+        option2.style.paddingBottom = "10px";
     }
 })
